a71b38fbdb0fde4a80825c68bed7af4ba9e13e58
FCL00 <flagahit00@gmail.com>
2025-06-26 01:25:50 +0800

    refractor: account store is refractor into options api instead of composition api

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -5,67 +5,134 @@ import type { Credentials } from '@/types'
 import { ElMessage } from 'element-plus'
 import { SHA256 } from 'crypto-js'
 
-export const useAccount = defineStore('accounts', () => {
-  const router = useRouter()
+export const useAccount = defineStore('accounts', {
 
-  const admin = {
-    username: import.meta.env.VITE_TEST_USER,
-    password: SHA256(import.meta.env.VITE_TEST_USER_PASSWORD).toString(),
-  }
+  state: () => ({
+    router: useRouter(),
+    admin: {
+      username: 'admin',
+      password: SHA256('admin123!').toString(),
+    },
+  }),
 
-  // create users
-  function onCreateUsers() {
-    try {
-      localStorage.setItem('isAuthenticated', 'false')
-      localStorage.setItem('students', JSON.stringify(users))
-      localStorage.setItem('accounts', JSON.stringify(admin))
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  actions: {
+    // creates users on mounted
+    onCreateUsers() {
+      try {
+        localStorage.setItem('isAuthenticated', 'false')
+        localStorage.setItem('students', JSON.stringify(users))
+        localStorage.setItem('accounts', JSON.stringify(this.admin))
+      } catch (error) {
+        ElMessage.error(error)
+      }
+    },
 
-  // check users credentials
-  function onLogin(credentials: Credentials) {
-    const account = JSON.parse(localStorage.getItem('accounts') || 'null')
-    const hashedPassword = SHA256(credentials.password).toString()
-    try {
-      if (account.username === credentials.username && account.password === hashedPassword) {
-        localStorage.setItem('isAuthenticated', 'true')
-        ElMessage.success('Successfully login')
-        router.push('/dashboard')
+    // check user credentials
+    onLogin(credentials: Credentials) {
+      const account = JSON.parse(localStorage.getItem('accounts') || 'null')
+      const hashedPassword = SHA256(credentials.password).toString()
+
+      try {
+        if (account?.username === credentials.username && account?.password === hashedPassword) {
+          localStorage.setItem('isAuthenticated', 'true')
+          ElMessage.success('Successfully login')
+          this.router.push('/dashboard')
+        } else {
+          ElMessage.error('Invalid Username or Password')
+          this.router.push('/')
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    // allows users to change their password into a new one
+    onForgotPassword(username: string, newpassword: string) {
+      const account = JSON.parse(localStorage.getItem('accounts') || 'null')
+
+      if (account?.username === username) {
+        const updatedAccount = {
+          ...account,
+          password: SHA256(newpassword).toString(),
+        }
+
+        localStorage.setItem('accounts', JSON.stringify(updatedAccount))
+        localStorage.setItem('isAuthenticated', 'false')
+
+        ElMessage.success('Successfully changed password')
+        this.router.push('/')
       } else {
-        ElMessage.error('Invalid Username or Password')
-        router.push('/')
+        ElMessage.error('Username not found')
       }
-    } catch (error) {
-      console.error(error)
     }
   }
-
-  // update password of the user
-  function onForgotPassword(username: string, password: string, newpassword: string) {
-    const account = JSON.parse(localStorage.getItem('accounts') || 'null')
-    const hashedPassword = SHA256(password).toString()
-
-    console.log('Input Credentials:', { username, hashedPassword, newpassword })
-    console.log('Stored Account:', account)
-
-    if (account.username === username && account.password === hashedPassword) {
-      console.log('Account is being updated')
-      const updatedAccount = {
-        ...account,
-        password: SHA256(newpassword).toString(),
-      }
-
-      localStorage.setItem('accounts', JSON.stringify(updatedAccount))
-      localStorage.setItem('isAuthenticated', 'false')
-
-      ElMessage.success('Successfully changed password')
-      router.push('/')
-    } else {
-      ElMessage.error('Invalid Username or Password')
-    }
-  }
-
-  return { onCreateUsers, onLogin, onForgotPassword }
 })
+
+
+
+// export const useAccount = defineStore('accounts', () => {
+//   const router = useRouter()
+  
+//   const admin = {
+//     username: 'admin',
+//     password: SHA256('admin123!').toString(),
+//   }
+
+//   // create users
+//   function onCreateUsers() {
+//     try {
+//       localStorage.setItem('isAuthenticated', 'false')
+//       localStorage.setItem('students', JSON.stringify(users))
+//       localStorage.setItem('accounts', JSON.stringify(admin))
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   }
+
+//   // check users credentials
+//   function onLogin(credentials: Credentials) {
+//     const account = JSON.parse(localStorage.getItem('accounts') || 'null')
+//     const hashedPassword = SHA256(credentials.password).toString()
+//     console.log(credentials);
+//     try {
+//       if (account.username === credentials.username && account.password === hashedPassword) {
+//         localStorage.setItem('isAuthenticated', 'true')
+//         ElMessage.success('Successfully login')
+//         router.push('/dashboard')
+//       } else {
+//         ElMessage.error('Invalid Username or Password')
+//         console.log('invalid :', credentials)
+//         router.push('/')
+//       }
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   }
+
+//   // update password of the user
+// function onForgotPassword(username: string, newpassword: string) {
+//   const account = JSON.parse(localStorage.getItem('accounts') || 'null')
+
+//   if (account?.username === username) {
+//     const updatedAccount = {
+//       ...account,
+//       password: SHA256(newpassword).toString(),
+//     }
+
+//     localStorage.setItem('accounts', JSON.stringify(updatedAccount))
+//     localStorage.setItem('isAuthenticated', 'false')
+
+//     ElMessage.success('Successfully changed password')
+//     router.push('/')
+//   } else {
+//     ElMessage.error('Username not found')
+//   }
+// }
+
+//   return { onCreateUsers, onLogin, onForgotPassword }
+// })
+
+
+
+
+
